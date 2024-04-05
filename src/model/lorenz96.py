e41ca84618228758ca0be2f9d3202a202fe1d220
Daples <daples1@gmail.com>
2024-04-05 11:55:51 +0200

    [ model ] Added n_states and observe to L96 model.

--- src/model/lorenz96.py
+++ src/model/lorenz96.py
@@ -19,6 +19,7 @@ class Lorenz96(ODEModel):
         self,
         initial_condition: NDArray,
         time_step: float,
+        n_states: int,
         forcing: float,
         system_cov: DynamicMatrix,
         observation_cov: DynamicMatrix,
@@ -27,6 +28,7 @@ class Lorenz96(ODEModel):
         super().__init__(
             initial_condition, time_step, system_cov, observation_cov, solver=solver
         )
+        self.n_states: int = n_states
         self.forcing: float = forcing
 
     def f(self, _: float, state: NDArray) -> NDArray:
@@ -38,3 +40,8 @@ class Lorenz96(ODEModel):
         for i in range(n_states):
             vec[i] = (x[(i + 1) % n_states] - x[i - 2]) * x[i - 1] - x[i] + self.forcing
         return vec
+
+    def observe(self, state: NDArray) -> NDArray:
+        """All states are observable."""
+
+        return state
