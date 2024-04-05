4c53192e31e97e953ec65879490aabbe2ed7dd9a
Daples <daples1@gmail.com>
2024-04-05 09:41:33 +0200

    [ utils ] Added default types for dynamic matrices and integrators.

---
+++ src/utils/_typing.py
@@ -0,0 +1,8 @@
+from typing import Callable
+from numpy.typing import NDArray
+
+Integrator = Callable[
+    [Callable[[float, NDArray], NDArray], NDArray, float, float, float],
+    tuple[NDArray, NDArray],
+]
+DynamicMatrix = Callable[[float], NDArray]
