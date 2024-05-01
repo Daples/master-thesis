413212dde59cfd77d3335bd4585edc0a1649385d
Daples <daples1@gmail.com>
2024-05-01 10:39:00 +0200

    [ utils ] Added default RNG.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,21 @@
 # get RMSE
 from numpy.linalg import inv
-from numpy.random import default_rng
+from numpy.random import Generator, default_rng
 from numpy.typing import NDArray
 
 default_generator = default_rng(123456789)
+
+
+def get_generator() -> Generator:
+    """It returns a default RNG.
+
+    Returns
+    -------
+    Generator
+        The RNG.
+    """
+
+    return default_generator
 
 
 def kalman_gain(P: NDArray, H: NDArray, R: NDArray) -> NDArray:
