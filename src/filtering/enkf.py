fdfb5451fca4ca796070fab6427589c397b8c509
Daples <daples1@gmail.com>
2024-05-01 10:39:34 +0200

    [ enkf ] Added cut off time logic.

--- src/filtering/enkf.py
+++ src/filtering/enkf.py
@@ -1,10 +1,13 @@
 from numpy.typing import NDArray
+from numpy.random import Generator
 
 from filtering import EnsembleFilter
 from utils import kalman_gain
 from utils._typing import DynamicMatrix
 
-from ..model import Model
+from model import Model
+
+import numpy as np
 
 
 class EnKF(EnsembleFilter):
@@ -23,12 +26,19 @@ class EnKF(EnsembleFilter):
         init_cov: NDArray,
         ensemble_size: int,
         H: DynamicMatrix,
+        generator: Generator | None = None,
     ) -> None:
-        super().__init__(model, init_state, init_cov, ensemble_size)
+        super().__init__(
+            model, init_state, init_cov, ensemble_size, generator=generator
+        )
         self.H: DynamicMatrix = H
 
     def compute_gain(self) -> NDArray:
         """Kalman gain."""
 
         t = self.current_time
-        return kalman_gain(self.forecast_cov, self.H(t), self.model.observation_cov(t))
+        if self.correct:
+            K = kalman_gain(self.forecast_cov, self.H(t), self.model.observation_cov(t))
+        else:
+            K = np.zeros((self.n_states, self.n_outputs))
+        return K
