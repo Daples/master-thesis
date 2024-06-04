654a9294ca886f62f71227b2ae19c275f1dd2921
Daples <daples1@gmail.com>
2024-06-04 11:36:49 +0200

    [ utils/_typing ] Added new type aliases.

--- src/utils/_typing.py
+++ src/utils/_typing.py
@@ -4,8 +4,17 @@ from numpy.typing import NDArray
 
 T = TypeVar("T")
 P = ParamSpec("P")
+
+# State types
+State: TypeAlias = NDArray
+Input: TypeAlias = NDArray
+Observation: TypeAlias = NDArray
+Time: TypeAlias = float
+InputFunction: TypeAlias = Callable[[float, State], Input]
+SystemDynamics: TypeAlias = Callable[[float, State], NDArray]
+
 Integrator: TypeAlias = Callable[
-    [Callable[[float, NDArray], NDArray], NDArray, float, float, float],
+    [SystemDynamics, NDArray, float, float, float],
     tuple[NDArray, NDArray],
 ]
 DynamicMatrix: TypeAlias = Callable[[float], NDArray]
