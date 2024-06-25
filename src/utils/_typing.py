710d95188953b4b283f47fdfb61a405709547f41
Daples <daples1@gmail.com>
2024-06-25 14:54:48 +0200

    [ utils/_typing ] Updated type aliases.

--- src/utils/_typing.py
+++ src/utils/_typing.py
@@ -12,9 +12,10 @@ Observation: TypeAlias = NDArray
 Time: TypeAlias = float
 InputFunction: TypeAlias = Callable[[float, State], Input]
 SystemDynamics: TypeAlias = Callable[[float, State], NDArray]
+InputSystem: TypeAlias = Callable[[float, State, Input], NDArray]
 
 Integrator: TypeAlias = Callable[
-    [SystemDynamics, NDArray, float, float, float],
+    [SystemDynamics, State, Time, Time, float, InputFunction],
     tuple[NDArray, NDArray],
 ]
 DynamicMatrix: TypeAlias = Callable[[float], NDArray]
